2c0810cf61f9636076e54957e1cb96b5e4058967
MichaelWiss <michael.wiss@gmail.com>
2017-04-02 14:27:57 -0500

    spread operator

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
+var Todo = require('Todo');
 
 var TodoList = React.createClass({
     render: function () {
     	var {todos} = this.props;
     	var renderTodos = () => {
-           return todo.map(() => {
-
+           return todos.map((todo) => {
+               return (
+                  <Todo key={todo.id} {...todo}/>
+               	);
            });
     	};
     	return (
