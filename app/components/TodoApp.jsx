89becb1bdcc27a6e4d9784cb5c2ad0b8c0a82372
MichaelWiss <michael.wiss@gmail.com>
2017-04-02 14:17:10 -0500

    Todo component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var TodoList = require('TodoList');
+
 
 var TodoApp = React.createClass({
 	getInitialState: function () {
@@ -15,9 +17,11 @@ var TodoApp = React.createClass({
        };
 	},
    render: function () {
+   	var {todos} = this.state;
+
    	return (
    		<div>
-   		TodoApp.jsx
+   		 <TodoList todos={todos}/>
    		</div>
    		)
    }
--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
 
 var TodoList = React.createClass({
+    render: function () {
+    	var {todos} = this.props;
+    	var renderTodos = () => {
+           return todo.map(() => {
 
+           });
+    	};
+    	return (
+            <div>
+            {renderTodos()}
+            </div>
+    		)
+    }
 });
 
 module.exports = TodoList;
