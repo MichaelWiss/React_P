3ba2b9731c6ce3478b1b257372c815ffb2743172
MichaelWiss <michael.wiss@gmail.com>
2017-04-04 17:45:11 -0500

    AddTodo.test

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+   it('should exist', () => {
+     expect(AddTodo).toExist();
+   });
+
+   it('should call onAddTodo prop with valid data', () => {
+
+   });
+});
