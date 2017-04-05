b25d7f7b02ba26d476ffaa2f967fbc5c0b9c5c34
MichaelWiss <michael.wiss@gmail.com>
2017-04-05 18:47:57 -0500

    search

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -1,1 +1,15 @@
 var React = require('react');
+
+var TodoSearch = React.createClass({
+	render: function () {
+		return (
+           <div>
+           <div></div>
+           <div></div>
+           </div>
+			)
+	}
+
+});
+
+module.exports = TodoSearch;
