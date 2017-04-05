698f8f9f4ccdeabe6ffa6b486031cf8e7df9f6b8
MichaelWiss <michael.wiss@gmail.com>
2017-04-05 14:48:17 -0500

    todoText

--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -12,13 +12,14 @@ describe('AddTodo', () => {
    });
 
    it('should call onAddTodo prop with valid data', () => {
+   	 var todoText = 'Check mail',
      var spy = expect.createSpy();
      var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
      var $el = $(ReactDOM.findDOMNode(addTodo));
 
-     addTodo.refs.todoText.value = 'Check mail';
+     addTodo.refs.todoText.value = todoText;
      TestUtils.Simulate.submit($el.find('form')[0]);
 
-     expect(spy).toHaveBeenCalledWith('Check mail');
+     expect(spy).toHaveBeenCalledWith(todoText);
    });
 });
