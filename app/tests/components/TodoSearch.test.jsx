7cf57e17729ea9e6954e1b626c11b98dc4cc30f1
MichaelWiss <michael.wiss@gmail.com>
2017-04-07 13:50:11 -0500

    test exist

--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -3,3 +3,17 @@ var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
+
+var TodoSearch = require('TodoSearch');
+
+describe('TodoSearch', () => {
+	it('should exist', () => {
+		expect(TodoSearch).toExist();
+	});
+
+	it('should call onSearch with entered input text', () => {
+
+	});
+
+	it('should')
+});
