787c16d82b186c52f4338261d00b4d9a9b2dc6e0
MichaelWiss <michael.wiss@gmail.com>
2017-04-11 14:46:58 -0500

    onToggle event handler

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -5,7 +5,9 @@ var Todo = React.createClass({
     	var {id, text, completed} = this.props;
     	
     	return (
-            <div>
+            <div onClick={() => {
+            	this.props.onToggle(id);
+            }}>
             <input type="checkbox" checked={completed}/>
             {text}
             </div>
