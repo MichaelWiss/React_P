d06371295e8a232ef77fc4eafdd3c94ac874d6b0
MichaelWiss <michael.wiss@gmail.com>
2017-04-13 12:58:22 -0500

    localstorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,8 +1,11 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
+
 
 
 var TodoApp = React.createClass({
@@ -10,27 +13,12 @@ var TodoApp = React.createClass({
        return {
         showCompleted: false,
         searchText: '',
-          todos: [
-             {
-             	id: uuid(),
-             	text: 'Walk the dog',
-              completed: false
-             }, {
-             	id: uuid(),
-             	text: 'Clean the yard',
-              completed: true
-             }, {
-             	id: uuid(),
-             	text: 'Work out',
-              completed: true
-             }, {
-             	id: uuid(),
-             	text: 'Do the laundry',
-              completed: false
-             }
-          ]
+          todos: TodoAPI.getTodos()
        };
 	},
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
+  },
 
   handleAddTodo: function (text) {
     this.setState({
