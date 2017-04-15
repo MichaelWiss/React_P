898b1b026ee7afac4aa814b66a41794e6daf2d2e
MichaelWiss <michael.wiss@gmail.com>
2017-04-14 20:19:23 -0500

    test bugs

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -3,11 +3,16 @@ var expect = require('expect');
 var TodoAPI = require('TodoAPI');
 
 describe('TodoAPI', () => {
+    beforeEach(() => {
+       localStorage.removeItem('todos');
+    });
+
 	it('should exist', () => {
 		expect(TodoAPI).toExist();
 	});
 
 	describe('setTodos', () => {
+	   it('should set valid todos array', () => {
        var todos = [{
           id: 23,
           test: 'test all files',
@@ -20,7 +25,7 @@ describe('TodoAPI', () => {
         expect(actualTodos).toEqual(todos);
 	});
 
-	it('should not set invalid todos array,' () => {
+	it('should not set invalid todos array', () => {
 		var badTodos = {a: 'b'};
 		TodoAPI.setTodos(badTodos);
 
