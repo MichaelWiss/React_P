9c12debb08e7ea327ab17908c1da915144613976
MichaelWiss <michael.wiss@gmail.com>
2017-04-17 13:57:07 -0500

    testing

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -73,6 +73,11 @@ describe('TodoAPI', () => {
         expect(filteredTodos.length).toBe(3);
      });
 
+     it('should return non-completed todos when showCompleted is false', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+        expect(filteredTodos.length).toBe(1);
+     })
+
      it('should sort by completed status', () => {
         var filteredTodos = TodoAPI.filterTodos(todos, true, '');
         expect(filteredTodos[0].completed).toBe(false);
