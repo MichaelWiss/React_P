d79a742843a3d35ed4fa459cd7eff190e0c6d925
MichaelWiss <michael.wiss@gmail.com>
2017-04-17 18:37:13 -0500

    completedAt

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -29,7 +29,8 @@ var TodoApp = React.createClass({
            id: uuid(),
            text: text,
            completed: false,
-           createdAt: moment().unix()
+           createdAt: moment().unix(),
+           completedAt: undefined
          }
        ]
     });
@@ -38,6 +39,7 @@ var TodoApp = React.createClass({
       var updatedTodos = this.state.todos.map((todo) => {
          if (todo.id === id) {
             todo.completed = !todo.completed;
+            todo.completedAt = todo.completed ? moment().unix() : undefined;
          }
 
          return todo;
