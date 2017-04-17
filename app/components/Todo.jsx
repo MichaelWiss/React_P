d42da069757568b44560cdf9e2dbdbf18672543f
MichaelWiss <michael.wiss@gmail.com>
2017-04-17 18:33:22 -0500

    added valid timestamp

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
     render: function () {
     	var {id, text, completed, createdAt} = this.props;
         var renderDate = () => {
-           var message = 'Created'
+           var message = 'Created ';
            var timestamp = createdAt;
 
            return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
