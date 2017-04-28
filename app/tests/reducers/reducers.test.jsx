56710a06d070dc0fecd8a58a50fa92b125ef4b3f
MichaelWiss <michael.wiss@gmail.com>
2017-04-28 13:10:28 -0500

    test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -2,6 +2,10 @@ var expect = require('expect');
 
 describe('Reducers', () => {
    describe('searchTextReducer', () => {
-      it('should set searchText', )
+     it('should set searchText', () => {
+      	var action = {
+      		type: 'SET_SEARCH_TEXT'
+      	}
+      });
    });
 });
