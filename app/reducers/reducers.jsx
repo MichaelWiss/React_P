14c44626c9739f5dc882935c485442aa86d075af
MichaelWiss <michael.wiss@gmail.com>
2017-04-28 15:32:52 -0500

    todosReducer

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
      switch (action.type) {
      	case 'SET_SEARCH_TEXT':
@@ -15,3 +18,19 @@ export var showCompletedReducer = (state = false, action) => {
           return state;
     }
 };
+
+export var todosReducer = (state = [], action) => {
+   switch (action.type) {
+   	case 'ADD_TODO':
+   	   return [
+   	   ...state,
+   	    {
+           id: uuid(),
+           text: action.text,
+           completed: false,
+           createdAt: moment().unix(),
+           completedAt: undefined
+         }
+   	 ]
+   }
+}; 
