0c23344c565ce79923b31f0e3bf44a6c270d5056
MichaelWiss <michael.wiss@gmail.com>
2017-04-28 13:23:02 -0500

    testing reducer

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,11 +1,17 @@
 var expect = require('expect');
 
+var reducers = require('reducers');
+
 describe('Reducers', () => {
    describe('searchTextReducer', () => {
      it('should set searchText', () => {
       	var action = {
-      		type: 'SET_SEARCH_TEXT'
-      	}
+      		type: 'SET_SEARCH_TEXT',
+      		searchText: 'dog'
+      	};
+      	var res = reducer.searchTextReducer('', action);
+
+      	expect(res).toEqual(action.searchText);
       });
    });
 });
