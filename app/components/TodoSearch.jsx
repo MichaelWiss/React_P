13c7beab1af0bbbfe0014f120fd381d2c485079b
MichaelWiss <michael.wiss@gmail.com>
2017-05-05 12:36:59 -0500

    dispatch searchText

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -3,17 +3,16 @@ var {connect} = require('react-redux');
 var actions = require('actions');
 
 export var TodoSearch = React.createClass({
-	handleSearch: function () {
-      var showCompleted = this.refs.showCompleted.checked;
-      var searchText = this.refs.searchText.value;
-
-      this.props.onSearch(showCompleted, searchText);
-	},
 	render: function () {
+    var {dispatch, showCompleted, searchText} = this.props;
+
 		return (
            <div className="container__header">
            <div>
-           	  <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+           	  <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={() => {
+                var searchText = this.refs.searchText.value;
+                dispatch (actions.setSearchText(searchText));
+              }}/>
            </div>
            <div>
            	  <label>
