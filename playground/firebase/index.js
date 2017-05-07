70e1176c7f67e1284166bb5560cabcb5b5495f29
MichaelWiss <michael.wiss@gmail.com>
2017-05-07 16:06:39 -0500

    firebase

---
+++ playground/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyB5HIEA4yVD78KOOjFrumvMECpOOGYP14U",
+    authDomain: "wiss-todo-app.firebaseapp.com",
+    databaseURL: "https://wiss-todo-app.firebaseio.com",
+    projectId: "wiss-todo-app",
+    storageBucket: "wiss-todo-app.appspot.com",
+    messagingSenderId: "409724155762"
+  };
+  firebase.initializeApp(config);
